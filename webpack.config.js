d87a3ce7114a6a7e7dbdc6a8e98f6a090c5e3be3
zoomzoom6 <tonyhuang107@live.ca>
2021-08-17 17:38:34 -0400

    fixed carousel

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
+const webpack = require("webpack")
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
-    ],
-    mode: 'development',
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
