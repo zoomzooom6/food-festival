37bce274e8722a672309afc9299d50c08b875bd0
zoomzoom6 <tonyhuang107@live.ca>
2021-08-19 17:28:13 -0400

    create pwa manifest for install on local device

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack")
 const path = require("path");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 
 module.exports = {
@@ -23,10 +24,10 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name (file) {
+              name(file) {
                 return "[path][name].[ext]"
               },
-              publicPath: function(url) {
+              publicPath: function (url) {
                 return url.replace("../", "/assets")
               }
             }
@@ -45,6 +46,21 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
   ],
   mode: "development"
